2c00523857ad9ea97fd3a34159c16dee5a8eac33
cqveman <trymemain.pb.hug@gmail.com>
2025-12-01 21:32:58 +0200

    Add registration page

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import json
 import os
+import time
+import datetime
 
-users = []
+users = [ ]
+
+def waitingAnimation(message, seconds, option=1):
+    symbols = []
+    if option == 1:
+        symbols = [
+            '▒▒▒▒▒▒▒▒▒▒ 0%', '██▒▒▒▒▒▒▒▒ 20%', '████▒▒▒▒▒▒ 40%',
+            '██████▒▒▒▒ 60%', '████████▒▒ 80%', '██████████ 100%'
+        ]
+    elif option == 2:
+        symbols = ['○', '◔', '◑', '◕', '●']
+    elif option == 3:
+        symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
+    print()
+    for i in symbols:
+        print(f'\r{message} {i}', end='')
+        time.sleep(seconds)
+    print('\n')
 
 while True:
     print('*──────────────────Central Bank of Yemen──────────────────*')
@@ -12,7 +31,87 @@ while True:
 
     # REGISTER
     if choice == 'register':
-        ...
+        print('\n#────────Sign-up────────#')
+        print('Please fill in your personal information: ')
+
+        fullName = input('Full Name: ').strip().capitalize()
+        dateOfBirth = input('Date of Birth (dd-mm-yyyy): ').strip()
+        gender = input('Gender: ').strip().capitalize()
+        job = input('Job: ').strip().capitalize()
+        address = input('Address (building #, Street Name, Neighbourhood, City-Postal Code): ').strip().capitalize()
+        phoneNum = input('Phone number (+967): ').strip()
+        email = input('Email (example@gmail.com): ').strip()
+
+        waitingAnimation('Validating Information', 1)
+
+        while True:
+            if fullName.replace(' ', '').replace('-','').isalpha():
+                break
+            fullName = input('\nInvalid Full Name. Only alphabetic characters allowed, try again:\n')
+
+        while True:
+            if len(dateOfBirth) == 10 and dateOfBirth[2] == '-' and dateOfBirth[5] == '-':
+                noDashesDate = dateOfBirth.replace('-', '')
+                if noDashesDate.isdigit():
+                    # Assume all months have 31 days
+                    if (
+                            1 <= int(noDashesDate[:2]) <= 31
+                            and 1 <= int(noDashesDate[2:4]) <= 12
+                            and 1900 <= int(noDashesDate[4:]) < datetime.datetime.now().year
+                    ):
+                        break
+            dateOfBirth = input('\nInvalid Date of Birth. Only integer values allowed in the format of "dd-mm-yyyy", try again:\n')
+
+        while True:
+            if gender == 'Male' or gender == 'Female':
+                break
+            gender = input('\nInvalid Gender. Enter either male or female:\n')
+
+        while True:
+            if job.replace(' ', '').isalpha():
+                break
+            job = input('\nInvalid Job. Only alphabetic characters allowed, try again:\n')
+
+        while True:
+            parts = address.split(', ')
+            if len(parts) == 4:
+                building = parts[0]
+                street = parts[1]
+                neighbourhood = parts[2]
+                city = parts[3]
+                if (
+                        building.isdigit()
+                        and street.replace(' ', '').replace('.', '').isalpha()
+                        and neighbourhood.replace(' ', '').isalpha()
+                ):
+                    cityParts = city.split('-')
+                    if len(cityParts) == 2 and cityParts[1].isdigit():
+                        break
+            address = input('\nInvalid Address. Please use this format "building #, Street Name, Neighbourhood, City-Postal Code":\n')
+
+        while True:
+            if phoneNum.isdigit() and len(phoneNum) == 9 and int(phoneNum[0]) == 7:
+                break
+            phoneNum = input('\nInvalid Phone Number. Omit +967 and enter 9 digits counting from "7":\n')
+
+        while True:
+            if email.count('@') == 1:
+                if email.rfind('.') > email.find('@'):
+                    # Assume min length is "a@b.c"
+                    if len(email) >= 5:
+                        break
+            email = input('\nInvalid Email. Please use this format "example@gmail.com":\n')
+
+        username = input('\nUsername: ').strip()
+        while True:
+            if username not in users:
+                print('Username Available ✔')
+                break
+            username = input('✘ Username is taken. Please try again:\n')
+
+        password = input('Password: ')
+
+
     elif choice == 'login':
         ...
     else:
