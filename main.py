dd21f3ac6e8fe3584394b2129b62f2e61aa8a484
cqveman <trymemain.pb.hug@gmail.com>
2025-12-04 13:24:18 +0200

    Create validators.py for input validation

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import json
 import os
 import time
-import datetime
 
-users = [ ]
+from validators import *
+
+users = [   ]
 
 fileName = 'users.json'
 if os.path.exists(fileName) and os.path.getsize(fileName) > 2:
     with open(fileName, 'r') as f:
-        accounts = json.load(f)
+        users = json.load(f)
+
+def saveToJson(fName):
+    with open(fName, 'w') as f:
+        json.dump(users, f, indent=2)
 
 def waitingAnimation(message, seconds, option=1):
     symbols = []
@@ -39,100 +44,37 @@ while True:
         print('\n#────────Sign-up────────#')
         print('Please fill in your personal information: ')
 
-        fullName = input('Full Name: ').strip().capitalize()
+        # User Input
+        fullName = input('Full Name: ').strip().title()
         dateOfBirth = input('Date of Birth (dd-mm-yyyy): ').strip()
         gender = input('Gender: ').strip().capitalize()
-        job = input('Job: ').strip().capitalize()
-        address = input('Address (building #, Street Name, Neighbourhood, City-Postal Code): ').strip().capitalize()
+        job = input('Job: ').strip().title()
+        address = input('Address (building #, Street Name, Neighbourhood, City-Postal Code): ').strip()
         phoneNum = input('Phone number (+967): ').strip()
         email = input('Email (example@gmail.com): ').strip()
 
         waitingAnimation('Validating Information', 1)
 
-        while True:
-            if fullName.replace(' ', '').replace('-','').isalpha():
-                break
-            fullName = input('\nInvalid Full Name. Only alphabetic characters allowed, try again:\n')
-
-        while True:
-            if len(dateOfBirth) == 10 and dateOfBirth[2] == '-' and dateOfBirth[5] == '-':
-                noDashesDate = dateOfBirth.replace('-', '')
-                if noDashesDate.isdigit():
-                    # Assume all months have 31 days
-                    if (
-                            1 <= int(noDashesDate[:2]) <= 31
-                            and 1 <= int(noDashesDate[2:4]) <= 12
-                            and 1900 <= int(noDashesDate[4:]) < datetime.datetime.now().year
-                    ):
-                        break
-            dateOfBirth = input('\nInvalid Date of Birth. Only integer values allowed in the format of "dd-mm-yyyy", try again:\n')
-
-        while True:
-            if gender == 'Male' or gender == 'Female':
-                break
-            gender = input('\nInvalid Gender. Enter either male or female:\n')
-
-        while True:
-            if job.replace(' ', '').isalpha():
-                break
-            job = input('\nInvalid Job. Only alphabetic characters allowed, try again:\n')
-
-        while True:
-            parts = address.split(', ')
-            if len(parts) == 4:
-                building = parts[0]
-                street = parts[1]
-                neighbourhood = parts[2]
-                city = parts[3]
-                if (
-                        building.isdigit()
-                        and street.replace(' ', '').replace('.', '').isalpha()
-                        and neighbourhood.replace(' ', '').isalpha()
-                ):
-                    cityParts = city.split('-')
-                    if len(cityParts) == 2 and cityParts[1].isdigit():
-                        break
-            address = input('\nInvalid Address. Please use this format "building #, Street Name, Neighbourhood, City-Postal Code":\n')
-
-        while True:
-            if phoneNum.isdigit() and len(phoneNum) == 9 and int(phoneNum[0]) == 7:
-                break
-            phoneNum = input('\nInvalid Phone Number. Omit +967 and enter 9 digits counting from "7":\n')
-
-        while True:
-            if email.count('@') == 1:
-                if email.rfind('.') > email.find('@'):
-                    # Assume min length is "a@b.c"
-                    if len(email) >= 5:
-                        break
-            email = input('\nInvalid Email. Please use this format "example@gmail.com":\n')
+        # Validations
+        fullName = validate(fullName, nameValidator, 'Invalid Full Name. Only alphabetic characters allowed, try again:')
+        dateOfBirth = validate(dateOfBirth, dofValidator, 'Invalid Date of Birth. Only integer values allowed in the formatB of "dd-mm-yyyy", try again:')
+        gender = validate(gender, genderValidator, 'Invalid Gender. Enter either male or female:')
+        job = validate(job, jobValidator, 'Invalid Job. Only alphabetic characters allowed, try again:')
+        address = validate(address, addressValidator, 'Invalid Address. Please use this format "building #, Street Name, Neighbourhood, City-Postal Code":')
+        phoneNum = validate(phoneNum, phoneValidator, 'Invalid Phone Number. Omit +967 and enter 9 digits counting from "7":')
+        email = validate(email, emailValidator, 'Invalid Email. Please use this format "example@gmail.com":')
 
         username = input('Username: ').strip()
         waitingAnimation('Checking Availability', 1, 3)
-
-        while True:
-            found = False
-            for user in users:
-                if user.get("username") == username:
-                    found = True
-                    break
-
-            if not found:
-                print('Username Available ✔')
-                break
-
-            username = input('✘ Username is taken. Please try again:\n')
+        username = validate(username, usernameValidator, '✘ Username is taken. Please try again:')
 
         password = input('Password: ')
         waitingAnimation('Validating Password', 1, 3)
-
-        while True:
-            if ' ' not in password:
-                if any(char.isdigit() for char in password):
-                    if len(password) >= 8:
-                        break
-            password = input('Invalid Password. must be at least 8 characters long, '
-                             'contain at least one number, and include no spaces.\nTry again: ')
+        password = validate(
+            password,
+            passwordValidator,
+            'Invalid Password. must be at least 8 characters long, contain at least one number, and include no spaces. Try again:'
+        )
 
         users.append(
             {
@@ -148,8 +90,7 @@ while True:
             }
         )
 
-        with open('users.json', 'w') as f:
-            json.dump(users,f,indent=2)
+        saveToJson(fileName)
 
         waitingAnimation('Registering User', 1, 2)
         print('Sign-up successful, your id is ' + str(len(users)))
